dfb502627740544e75efdd165763049844a8d261
Israel Neves <isrmicha@gmail.com>
2020-01-07 21:45:59 -0300

    took

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import exec from './worker'
-import styled from 'styled-components'
 
-const payload = Array(1000)
+const payload = Array(10000)
   .fill()
   .map(() => Math.round(Math.random() * 10000))
 
 const App = () => {
   const [tasks, setTasks] = useState(null)
-  const [rotate, setRotate] = useState(0)
-  const withoutWebWorkers = () => setTasks(payload.map(data => data ** 2))
-  const withWebWorkers = () =>
-    exec(Object.assign([], payload), arr => arr.map(data => data ** 2))
-      .then(res => setTasks(res))
-      .catch(err => console.log(err))
-  useEffect(() => {
-    setTimeout(() => setRotate(rotate + 15), 25)
-  }, [rotate])
+  const [time, setTime] = useState(null)
+  const withoutWebWorkers = () => {
+    const initialTime = Date.now()
+    setTasks(payload.map(data => data ** 2))
+    setTime(Date.now() - initialTime)
+  }
+  const withWebWorkers = () => {
+    const initialTime = Date.now()
+    exec(Object.assign([], payload), arr => arr.map(data => data ** 2)).then(
+      res => {
+        setTime(Date.now() - initialTime)
+        setTasks(res)
+      }
+    )
+  }
 
   return (
     <div>
       <p>Choose :</p>
-      <StyledDiv rotate={rotate} />
       <button onClick={withoutWebWorkers} style={{ margin: 10 }}>
         Main Thread (withoutWebWorkers)
       </button>
       <button onClick={withWebWorkers}>Multiple Thread (withWebWorkers)</button>
+      {time && <p>tooks {time} ms</p>}
       {tasks &&
         tasks.map((task, index) => <p key={`${task}-${index}`}>{task}</p>)}
     </div>
@@ -33,11 +38,3 @@ const App = () => {
 }
 
 export default App
-
-const StyledDiv = styled.div`
-  width: 250px;
-  height: 50px;
-  transform: rotate(${({ rotate }) => rotate}deg);
-  background-color: red;
-  margin: 100px;
-`
